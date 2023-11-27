5c1f196b4ee7b63d4c167a0bad9ec783f0eb6095
isabellafsr <you@example.com>
2023-11-27 17:52:18 -0300

    byu

--- index.js
+++ index.js
@@ -16,24 +16,42 @@ if(process.env.OMG === "PROD"){
 
 const modelodeUsuario = mongoose.model('contas', new mongoose.Schema({
     email: String,
-    password: String
+    senha: String,
+    eadmin: Boolean
 }))
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/aaaa') // process.env.URL
  .then(()=>{
 
-app.post('/get/', async (req,res)=>{
-    const usuarioEncontrado = await modelodeUsuario.findOne({email: req.body.email, password: req.body.password})
+app.post('/login/', async (req,res)=>{
+    const usuarioEncontrado = await modelodeUsuario.findOne({email: req.body.email, senha: req.body.senha})
     if(usuarioEncontrado === null){
-        return res.send('conta não existe')
+        return res.send('Usuário não encontrado')
     }
     res.send(usuarioEncontrado)
 })
+
+app.post('/getProdutos/', async (req,res)=>{
+    const produtoEncontrado = await modelodeUsuario.findOne({violãoR$61900: req.body.violãoR$61900, bateriaR$320000: req.body.bateriaR$320000, guitarraR$59800: req.body.guitarraR$59800, saxofoneR$194000: req.body.saxofoneR$194000})
+    if(produtoEncontrado === null){
+        return res.send('Produto não encontrado')
+    }
+    res.send(produtoEncontrado)
+})
   
-app.post('/post',async (req,res) =>{
-    const usuarioCriado = await modelodeUsuario.create({email: req.body.email, password: req.body.password})
+app.post('/cadastro',async (req,res) =>{
+    const usuarioCriado = await modelodeUsuario.create({email: req.body.email, senha: req.body.senha, endereço: req.body.endereço, cidade: req.body.cidade, cep: req.body.cep, estado: req.body.estado, telefone: req.body.telefone, admin: req.body.admin})
     res.send(usuarioCriado)
+})
+
+app.post('/admin',async (req,res) =>{
+    const usuarioEncontrado = await modelodeUsuario.findOne({email: req.body.email, senha: req.body.senha})
+    if (usuarioEncontrado.eadmin === null) {
+        return res.send('')
+    }
+    const produtoCriado = await modelodeUsuario.create({violãoR$61900: req.body.violãoR$61900, bateriaR$320000: req.body.bateriaR$320000, guitarraR$59800: req.body.guitarraR$59800, saxofoneR$194000: req.body.saxofoneR$194000})
+    res.send(produtoCriado)
 })
 
 app.put('/put', async (req,res)=>{
